43d748f408285db218cded0b7736f79118e317bb
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-20 16:09:44 -0500

    have to redo this code, will make branch david2

--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,14 @@
 require_relative '../config/environment'
 
 def determine_user
-  puts "Please enter your name"
+  puts "Please enter your username: "
   name = gets.chomp
   Writer.find_by(username: name)
 end
 
-def welcome
+def welcome(user)
   pastel = Pastel.new
-  puts pastel.decorate("Welcome to your Journal Explorer!", :green, :bold)
+  puts pastel.decorate("Welcome to your Journal Explorer, #{user.username}!", :green, :bold)
 end
 
 def divider
@@ -25,11 +25,32 @@ end
 
   
 
-welcome 
 user = determine_user
-puts user.entries.first.body
+welcome(user)
 divider
-#first_menu_box
+puts "What would you like to do?"
+first_menu_box
+
+choice = gets.chomp.to_i
+
+if choice == 1
+
+  puts "What do you want to call this journal?"
+  journal_name = gets.chomp
+  puts "Do you want to add a subject? (Y/N) "
+  choice = gets.chomp.upcase
+  if choice == "Y"
+    puts "What subject should this journal be?"
+    subject = gets.chomp.capitalize
+    user.create_journal(journal_name, subject)
+  else
+    user.create_journal(journal_name)
+  end 
+
+elsif choice == 2
+  journal_names = user.journals.uniq.map{ |journal| journal.name}
+  binding.pry
+end
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
+require 'pry'
+
 Writer.destroy_all
 Journal.destroy_all
 Entry.destroy_all
 
-writer1 = Writer.create(username: "David")
-writer2 = Writer.create(username: "Wintana")
-writer3 = Writer.create(username: "Steve")
+writer1 = Writer.create(username: "David", password: "abc")
+writer2 = Writer.create(username: "Wintana", password: "xyz")
+#writer3 = Writer.create(username: "Steve")
 
-writer1.create_journal("Journal 1")
-writer2.create_journal("Journal 2")
+j1 = Journal.create(name: "Journal 1", subject: "First Journal")
+j2 = Journal.create(name: "Journal 2", subject: "Second Journal")
+
+e1 = Entry.create(title: "Entry1 ", writer_id: writer1.id, journal_id: j1.id, body: 'This is the first entry')
+e2 = Entry.create(title: "Entry 2", writer_id: writer2.id, journal_id: j2.id, body: 'This is the second entry')
 
 
 #writer1.write_entry(j1, "This is my first entry", "Entry 1")
 #writer2.write_entry(j1, "This is the second entry in this journal", "Entry2")
 #writer1.write_entry(j1, "This is the third entry in this journal", "Entry3")
+binding.pry
+0

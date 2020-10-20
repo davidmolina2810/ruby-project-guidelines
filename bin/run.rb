fa57fdc6cd48946ace753ac77a2d9341cdee09be
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-20 14:20:22 -0500

    made #determine_user, #welcome, #divder, #first_menu_box

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,35 @@
 require_relative '../config/environment'
 
+def determine_user
+  puts "Please enter your name"
+  name = gets.chomp
+  Writer.find_by(username: name)
+end
+
+def welcome
+  pastel = Pastel.new
+  puts pastel.decorate("Welcome to your Journal Explorer!", :green, :bold)
+end
+
+def divider
+  puts "==========================="
+end
+
+def first_menu_box 
+  box = TTY::Box.frame "1. Create new Journal", "2. Open a Journal", "3. Delete Journal", align: :left
+  print box 
+end
+
+def create_writer(name)
+end
+
+  
+
+welcome 
+user = determine_user
+puts user.entries.first.body
+divider
+#first_menu_box
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ writer1 = Writer.create(username: "David")
 writer2 = Writer.create(username: "Wintana")
 writer3 = Writer.create(username: "Steve")
 
-j1 = Journal.create(name: "Journal 1")
-j2 = Journal.create(name: "Journal 2")
-j3 = Journal.create(name: "Journal 3")
+writer1.create_journal("Journal 1")
+writer2.create_journal("Journal 2")
 
-writer1.write_entry(j1, "This is my first entry", "Entry 1")
-writer2.write_entry(j1, "This is the second entry in this journal", "Entry2")
-writer1.write_entry(j1, "This is the third entry in this journal", "Entry3")
+
+#writer1.write_entry(j1, "This is my first entry", "Entry 1")
+#writer2.write_entry(j1, "This is the second entry in this journal", "Entry2")
+#writer1.write_entry(j1, "This is the third entry in this journal", "Entry3")

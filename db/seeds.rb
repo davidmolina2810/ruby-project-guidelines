90aac1a0ab717256dd6c2ea872e68e547362ae10
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-19 21:28:29 -0500

    made new_body and new_title optional args in Writer#update_entry, and associated to journal and writer

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,4 @@ j1 = Journal.create(name: "Journal 1")
 j2 = Journal.create(name: "Journal 2")
 j3 = Journal.create(name: "Journal 3")
 
-e1 = Entry.create
-e2 = Entry.create
-e3 = Entry.create
-
-writer1.entries << e1
-j1.entries << e1
+writer1.write_entry(j1, "This is my first entry")
--- lib/writer.rb
+++ lib/writer.rb
@@ -2,14 +2,35 @@ class Writer < ActiveRecord::Base
   has_many :entries
   has_many :journals, through: :entries
 
-  def write_entry(journal, body)
+  def write_entry(journal, body) # create new entry in journal 
     entry = Entry.create(body: body)
     journal.entries << entry
     entries << entry
     entry
   end
+  
+  def entry_by_title(title)
+    Entry.find_by(title: title)
+  end
 
-  def update_entry(entry, new_body)
-    entry.update(body: new_body)
+  def update_entry(entry, journal, new_body = nil, new_title = nil) # update given entry in journal
+    if new_body && new_title
+      entry.update(body: new_body)
+      entry.update(title: new_title)
+    elsif new_body && !new_title
+      entry.update(body: new_body)
+    elsif !new_body && new_title
+      entry.update(title: new_title)
+    else 
+      return "Must pass either new_body and/or new_title arguments"
+    end
+    journal.entries << entry
+    self.entries << entry
+    entry
+  end
+
+  def delete_entry(entry)
+    entry.destroy
+    self.entries
   end
 end 
